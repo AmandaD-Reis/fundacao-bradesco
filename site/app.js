21ed90189775b0045f7e4083792904f92d922c11
AMANDA REIS <89555917+AmandaD-Reis@users.noreply.github.com>
2023-04-14 14:01:20 -0300

    criando um site simples com html, css, javascript

---
+++ site/app.js
@@ -0,0 +1,21 @@
+'use strict'
+
+const switcher = document.querySelector('button');
+
+
+switcher.addEventListener('click', function() {
+
+    document.body.classList.toggle("dark-theme")
+
+    var className = document.body.className;
+
+    if(className == "light-theme"){
+        this.textContent = "Dark";}
+        else {
+            this.textContent = "Ligth";
+        }
+
+        console.log('current class name: '+ className);    
+
+});
+
